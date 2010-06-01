9ffa4b16bdb00ccf0c9cadaf4d472b264a49266c
Jason Maur <jmaur@dawsoncollege.qc.ca>
2010-06-01 07:10:22 -0400

    Added create course buttons in block_cegep.php

--- block_cegep.php
+++ block_cegep.php
@@ -1,36 +1,45 @@
 <?php
 
 include_once($CFG->dirroot . '/course/lib.php');
+require_once($CFG->dirroot . '/blocks/cegep/lib.php');
 
 class block_cegep extends block_list {
     function init() {
         $this->title = get_string('admincegep', 'block_cegep');
-        $this->version = 20090603;
+        $this->version = 20100527;
     }
     
     function has_config() {return true;}
 
     function get_content() {
-        global $USER, $COURSE;
+		global $USER, $COURSE;
 
         if ($this->content !== NULL) {
             return $this->content;
         }
 
-        $context = get_context_instance(CONTEXT_COURSE, $COURSE->id);
-        if (!has_capability('moodle/course:update', $context)) {  // Just return
-            return $this->content;
-        }
+		$this->content = new stdClass;
 
-        $metachild = is_object(get_record('course_meta', 'child_course', $COURSE->id));
-        $this->content = new stdClass;
+		$context = get_context_instance(CONTEXT_COURSE, $COURSE->id);
 
-		$this->content->items[] = '<a href="../blocks/cegep/block_cegep_enrolment.php?id='.$COURSE->id.'">'.get_string('enrolment', 'block_cegep').'</a>';
-		$this->content->icons[] = "&bull;";
+		if (strpos($_SERVER['PHP_SELF'], '/my') !== false) {
 
-        $this->content->footer = '';
-
-        return $this->content;
+			if (isset($_POST['cegepcoursenumber'])) {
+				cegep_local_create_course($_POST['cegepcoursenumber'], $_POST['cegepcoursetitle'], $_POST['cegepcoursesection'], $_POST['cegepcoursetrimester']);
+			}
+			$this->content->items = cegep_local_get_create_course_buttons();
+			$this->content->icons[] = null; // don't show any bullet
+		}
+		else {
+			if (!has_capability('moodle/course:update', $context)) {  // Just return
+				return $this->content;
+			}
+			// in course context
+			$this->content->items[] = '<a href="../blocks/cegep/block_cegep_enrolment.php?id='.$COURSE->id.'">'.get_string('enrolment', 'block_cegep').'</a>';
+			$this->content->icons[] = "&bull;";
+		}
+		$this->content->footer = '';
+		return $content;
     }
 }
 
